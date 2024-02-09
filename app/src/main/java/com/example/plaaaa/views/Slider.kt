761b334d56849b235790d1e2782f7a78950fdfeb
nezuko163 <Nezuko156@gmail.com>
2024-02-09 17:37:24 +0300

    Create Slider.kt

---
+++ app/src/main/java/com/example/plaaaa/views/Slider.kt
@@ -0,0 +1,13 @@
+package com.example.plaaaa.views
+
+import com.google.android.material.slider.Slider
+
+class onTouch : Slider.OnSliderTouchListener {
+    override fun onStartTrackingTouch(slider: Slider) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStopTrackingTouch(slider: Slider) {
+        TODO("Not yet implemented")
+    }
+}
